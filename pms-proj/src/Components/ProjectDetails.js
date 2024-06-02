31e549fc6c8afc95d1783731adc7b1f203bb60c9
AsharAmir <ashharamir@gmail.com>
2024-06-02 14:45:38 +0500

    new additions, scrum!

--- pms-proj/src/Components/ProjectDetails.js
+++ pms-proj/src/Components/ProjectDetails.js
@@ -10,8 +10,7 @@ const GlobalStyle = createGlobalStyle`
     ::-webkit-scrollbar {
         display: none;
     }
-
-    /* Hide scrollbar for IE, Edge and Firefox */
+    
     body {
         -ms-overflow-style: none;  /* IE and Edge */
         scrollbar-width: none;  /* Firefox */
