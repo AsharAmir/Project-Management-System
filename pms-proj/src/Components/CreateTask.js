10782dcff006d8182aba372e1bf26739f64d8128
AsharAmir <ashharamir@gmail.com>
2024-06-05 15:26:02 +0500

    updated ui of createtask form

--- pms-proj/src/Components/CreateTask.js
+++ pms-proj/src/Components/CreateTask.js
@@ -3,7 +3,7 @@ import { styled } from '@mui/system';
 import { Box, Button, Typography, Select, MenuItem, TextField, Paper } from '@mui/material';
 import Sidebar from './Sidebar';
 import axios from 'axios';
-import {createGlobalStyle} from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -44,10 +44,47 @@ const StyledButton = styled(Button)(({ theme }) => ({
     '&:hover': {
         backgroundColor: '#0A2744',
     },
-    border: '1px solid #095BAD', // Add border
-    color: '#fff', // Text color
+    border: '1px solid #095BAD',
+    color: '#fff',
     backgroundColor: 'rgba(82,211,177,0.07)',
 }));
+
+const StyledSelect = styled(Select)({
+    '& .MuiOutlinedInput-notchedOutline': {
+        borderColor: 'rgba(255, 255, 255, 0.5)',
+    },
+    '&:hover .MuiOutlinedInput-notchedOutline': {
+        borderColor: 'rgba(255, 255, 255, 1)',
+    },
+    '& .MuiSelect-icon': {
+        color: 'rgba(255, 255, 255, 0.5)',
+    },
+    '& .MuiSelect-iconOpen': {
+        color: 'rgba(255, 255, 255, 1)',
+    },
+    color: 'rgba(255, 255, 255, 0.87)',
+});
+
+const StyledTextField = styled(TextField)({
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: 'rgba(255, 255, 255, 0.5)',
+        },
+        '&:hover fieldset': {
+            borderColor: 'rgba(255, 255, 255, 1)',
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: 'rgba(255, 255, 255, 1)',
+        },
+        color: 'rgba(255, 255, 255, 0.87)',
+    },
+    '& .MuiInputLabel-outlined': {
+        color: 'rgba(255, 255, 255, 0.5)',
+    },
+    '& .MuiInputLabel-outlined.Mui-focused': {
+        color: 'rgba(255, 255, 255, 1)',
+    },
+});
 
 const AddTaskToProject = () => {
     const [projects, setProjects] = useState([]);
@@ -58,7 +95,6 @@ const AddTaskToProject = () => {
     const [startDate, setStartDate] = useState('');
     const [endDate, setEndDate] = useState('');
     const [error, setError] = useState(null);
-    const [taskStatus, setStatus] = useState('');
 
     useEffect(() => {
         fetchProjects();
@@ -83,8 +119,6 @@ const AddTaskToProject = () => {
 
     const handleProjectChange = (event) => {
         setSelectedProject(parseInt(event.target.value, 10));
-        console.log(parseInt(event.target.value, 10));
-        console.log(selectedProject, typeof selectedProject);
     };
 
     const handleAddTask = async (e) => {
@@ -99,18 +133,17 @@ const AddTaskToProject = () => {
         }
 
         const taskData = {
-            taskName: taskName,
-            description: description,
-            priority: priority,
-            startDate: startDate,
-            endDate: endDate,
+            taskName,
+            description,
+            priority,
+            startDate,
+            endDate,
             projectID: selectedProject,
             taskStatus: "Pending"
         };
-        console.log(taskData);
+
         try {
             const res = await axios.post('http://localhost:3000/api/tasks/addTask', taskData);
-            console.log('Task created', res.data);
             alert('Task added successfully');
             setTaskName('');
             setDescription('');
@@ -119,7 +152,6 @@ const AddTaskToProject = () => {
             setEndDate('');
         } catch (error) {
             console.error(error);
-            console.error('Server responded with error:', error.response.status, JSON.stringify(error.response.data));
             setError('Error adding task: ' + JSON.stringify(error.response.data));
         }
     };
@@ -131,87 +163,87 @@ const AddTaskToProject = () => {
     return (
         <>
             <GlobalStyle />
-        <Root>
-            <Sidebar />
-            <Content>
-                <ContentContainer>
-                    <Typography variant="h6" gutterBottom fontWeight="bold">
-                        Add Task to Project
-                    </Typography>
-                    <Select
-                        value={selectedProject}
-                        onChange={handleProjectChange}
-                        fullWidth
-                        displayEmpty
-                        inputProps={{ 'aria-label': 'Select Project' }}
-                    >
-                        <MenuItem value="" disabled>
-                            Select Project
-                        </MenuItem>
-                        {projects.map((project) => (
-                            <MenuItem key={project.projectId} value={project.projectId}>
-                                {project.projectName}
+            <Root>
+                <Sidebar />
+                <Content>
+                    <ContentContainer>
+                        <Typography variant="h6" gutterBottom fontWeight="bold">
+                            Add Task to Project
+                        </Typography>
+                        <StyledSelect
+                            value={selectedProject}
+                            onChange={handleProjectChange}
+                            fullWidth
+                            displayEmpty
+                            inputProps={{ 'aria-label': 'Select Project' }}
+                        >
+                            <MenuItem value="" disabled>
+                                Select Project
                             </MenuItem>
-                        ))}
-                    </Select>
-                    <TextField
-                        label="Task Name"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        value={taskName}
-                        onChange={(e) => setTaskName(e.target.value)}
-                    />
-                    <TextField
-                        label="Description"
-                        variant="outlined"
-                        fullWidth
-                        margin="normal"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                    />
-                    <Select
-                        value={priority}
-                        onChange={(e) => setPriority(e.target.value)}
-                        fullWidth
-                        displayEmpty
-                        inputProps={{ 'aria-label': 'Select Priority' }}
-                    >
-                        <MenuItem value="low">Low</MenuItem>
-                        <MenuItem value="normal">Normal</MenuItem>
-                        <MenuItem value="high">High</MenuItem>
-                    </Select>
-                    <TextField
-                        label="Start Date"
-                        variant="outlined"
-                        type="date"
-                        fullWidth
-                        margin="normal"
-                        value={startDate}
-                        onChange={(e) => setStartDate(e.target.value)}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField
-                        label="End Date"
-                        variant="outlined"
-                        type="date"
-                        fullWidth
-                        margin="normal"
-                        value={endDate}
-                        onChange={(e) => setEndDate(e.target.value)}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <StyledButton onClick={handleAddTask} variant="contained" color="primary">
-                        Add Task
-                    </StyledButton>
-                </ContentContainer>
-            </Content>
-        </Root>
-            </>
+                            {projects.map((project) => (
+                                <MenuItem key={project.projectId} value={project.projectId}>
+                                    {project.projectName}
+                                </MenuItem>
+                            ))}
+                        </StyledSelect>
+                        <StyledTextField
+                            label="Task Name"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={taskName}
+                            onChange={(e) => setTaskName(e.target.value)}
+                        />
+                        <StyledTextField
+                            label="Description"
+                            variant="outlined"
+                            fullWidth
+                            margin="normal"
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                        />
+                        <StyledSelect
+                            value={priority}
+                            onChange={(e) => setPriority(e.target.value)}
+                            fullWidth
+                            displayEmpty
+                            inputProps={{ 'aria-label': 'Select Priority' }}
+                        >
+                            <MenuItem value="low">Low</MenuItem>
+                            <MenuItem value="normal">Normal</MenuItem>
+                            <MenuItem value="high">High</MenuItem>
+                        </StyledSelect>
+                        <StyledTextField
+                            label="Start Date"
+                            variant="outlined"
+                            type="date"
+                            fullWidth
+                            margin="normal"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <StyledTextField
+                            label="End Date"
+                            variant="outlined"
+                            type="date"
+                            fullWidth
+                            margin="normal"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <StyledButton onClick={handleAddTask} variant="contained" color="primary">
+                            Add Task
+                        </StyledButton>
+                    </ContentContainer>
+                </Content>
+            </Root>
+        </>
     );
 };
 
